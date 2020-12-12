79788ffa80fd9a43b41c260c058181d124d8f1ce
anton_shipkov <Anton_Shipkov@tut.by>
2020-12-12 14:58:21 +0300

    fifth commit

--- HomeWork_loops_2_1_5.java
+++ HomeWork_loops_2_1_5.java
@@ -1,13 +1,25 @@
-package HomeWork1;
+package HomeWork1; //Делал до видео, не додумался дополнить вторым циклом
 
 public class HomeWork_loops_2_1_5 {
     public static void main(String[] args) {
-        for (int i=2; i<=9; i++) {
-            for (int j=1; j<=10; j++)
-                System.out.println(i+" * "+j+" = "+i*j);
-            System.out.println();
+        System.out.println("Таблица умножения");
+        for (int i=1; i<=10; i++) {
+            for (int j=2; j<=5; j++) {
+                System.out.print(j + " * " + i + " = " + (i * j) + "\t");
+            }
+            System.out.println("\n");
         }
+        System.out.println();
+        for (int i=1; i<=10; i++) {
+            for (int j=6; j<=9; j++) {
+                System.out.print(j + " * " + i + " = " + (i * j) + "\t");
+            }
+            System.out.println("\n");
+        }
+
+
     }
+
 
 
 
