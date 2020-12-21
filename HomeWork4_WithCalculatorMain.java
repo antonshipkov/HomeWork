dafcae31a12e861ed5954a390088ff0e1b01a5f1
anton_shipkov <Anton_Shipkov@tut.by>
2020-12-21 22:01:06 +0300

    eighth commit

---
+++ HomeWork4_WithCalculatorMain.java
@@ -0,0 +1,6 @@
+public class HomeWork4_WithCalculatorMain {
+    public static void main(String[] args) {
+        System.out.print("Выражение = ");
+        System.out.printf("%.2f", (4.1 + 15 * 7 + (Math.pow((double) 28 / 5, 2))));
+    }
+}
